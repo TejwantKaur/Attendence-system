976c98583bb7a2498e27a5453399caa5107eac32
Tejwant Kaur <114721213+TejwantKaur@users.noreply.github.com>
2024-05-05 19:54:37 +0530

    Delete 04_attendance_app directory

--- 04_attendance_app/home.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title='Attendance System', layout='wide')
-st.header('Attendance System Using Face Recog')
-
-with st.spinner('Loading Models and connecting to Redis db...'):
-    import face_recog
-
-st.success('Model loaded and Redis db Successfully Connected')
